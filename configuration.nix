3b79cb2588bf13e94b0f0ff3f73198c2a2046890
root <root@nixos>
2022-12-10 17:43:06 +0100

    Config tlp

--- configuration.nix
+++ configuration.nix
@@ -84,7 +84,16 @@
   };
 
   services.power-profiles-daemon.enable = false;
-  services.tlp.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      START_CHARGE_THRESH_BAT0 = "75";
+      STOP_CHARGE_THRESH_BAT0 = "80";
+    };
+  };
+
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;

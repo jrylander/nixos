ff4fc1cf035cc86975ef865aa0964dec6fd3f701
Johan Rylander <johan@rylander.cc>
2022-12-29 22:44:41 +0100

    Add syncthing and borg client

--- configuration.nix
+++ configuration.nix
@@ -42,6 +42,38 @@
     LC_TIME = "sv_SE.UTF-8";
   };
 
+  networking.firewall.allowedTCPPorts = [ 8384 22000];
+  networking.firewall.allowedUDPPorts = [ 22000 21027];
+
+  services = {
+    syncthing = {
+      enable = true;
+      guiAddress = "0.0.0.0:8384";
+      user = "syncthing";
+      dataDir = "/syncthing";
+      configDir = "/syncthing/config";
+    };
+  };
+
+  users.users.syncthing = {
+    home = "/syncthing";
+  };
+
+  services.borgbackup.jobs = {
+    borgnix = {
+      paths = [ "/syncthing" ];
+      doInit = true;
+      repo =  "borg@borgnix.rylander.cc:/borg/repos/syncthing" ;
+      encryption = {
+        mode = "repokey-blake2";
+        passCommand = "cat /run/keys/borgbackup_passphrase";
+      };
+      environment = { BORG_RSH = "ssh -i /run/keys/id_ed25519_syncthing-server"; };
+      compression = "auto,lzma";
+      startAt = "hourly";
+    };
+  };
+
 
   users.users.jrylander = {
     isNormalUser = true;

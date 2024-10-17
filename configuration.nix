cadbcaf2ff3267d0698b00a4be44289376c2c8eb
Johan Rylander <johan@rylander.cc>
2024-10-17 18:44:59 +0200

    Remove netdata

--- configuration.nix
+++ configuration.nix
@@ -52,24 +52,6 @@
 
   services.qemuGuest.enable = true;
 
-  services.netdata = {
-    enable = true;
-
-    config = {
-      global = {
-        # uncomment to reduce memory to 32 MB
-        #"page cache size" = 32;
-
-        # update interval
-        "update every" = 15;
-      };
-      ml = {
-        # enable machine learning
-        "enabled" = "yes";
-      };
-    };
-  };
-
   services.borgbackup.jobs = {
     borgnix = {
       paths = [ "/home/johan" "/home/gunnel" ];
